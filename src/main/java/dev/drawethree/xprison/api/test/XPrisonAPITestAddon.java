522624884afabb5466553f6234531151004a5dac
Jan Kluka <jan.kluka@enghouse.com>
2025-07-15 21:16:56 +0200

    1.0-SNAPSHOT Updated to latest API usage

--- src/main/java/dev/drawethree/xprison/api/test/XPrisonAPITestAddon.java
+++ src/main/java/dev/drawethree/xprison/api/test/XPrisonAPITestAddon.java
@@ -1,44 +1,50 @@
 package dev.drawethree.xprison.api.test;
 
 import dev.drawethree.xprison.api.XPrisonAPI;
+import dev.drawethree.xprison.api.addons.XPrisonAddon;
 import dev.drawethree.xprison.api.enchants.model.XPrisonEnchantment;
 import dev.drawethree.xprison.api.test.enchant.DiamondGiverEnchant;
 import dev.drawethree.xprison.api.test.enchant.InvisibilityEnchant;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A test plugin for the XPrisonAPI.
+ * A test addon for the XPrisonAPI.
  * <p>
- * This plugin demonstrates how to register and manage custom enchantments
+ * This addon demonstrates how to register and manage custom enchantments
  * using the XPrison enchantments API. On plugin enable, it loads configuration
  * files for the custom enchants, registers them with the XPrison API,
  * and unregisters them on plugin disable.
+ *
+ * IMPORTANT!
+ * Jar file should be placed under plugins/X-Prison/addons
  */
-public final class XPrisonAPITest extends JavaPlugin {
+public final class XPrisonAPITestAddon implements XPrisonAddon {
 
-    private static XPrisonAPITest INSTANCE;
+    private static XPrisonAPITestAddon INSTANCE;
     private XPrisonAPI api;
     private List<XPrisonEnchantment> customEnchants;
+    private File dataFolder;
 
     /**
      * Gets the singleton instance of this plugin.
      *
      * @return the instance of XPrisonAPITest
      */
-    public static XPrisonAPITest getINSTANCE() {
+    public static XPrisonAPITestAddon getINSTANCE() {
         return INSTANCE;
     }
 
     /**
-     * Called when the plugin is enabled.
+     * Called when the addon is enabled.
      * Initializes the API, copies default config files for enchantments,
      * initializes enchantment instances, loads their configuration,
      * and registers them with the API.
@@ -47,6 +53,11 @@ public final class XPrisonAPITest extends JavaPlugin {
     public void onEnable() {
         INSTANCE = this;
         api = XPrisonAPI.getInstance();
+
+        this.dataFolder = new File(Bukkit.getPluginManager().getPlugin("X-Prison").getDataFolder(), "addons-data/XPrisonAPITestAddon");
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
 
         copyDefaultEnchantConfigurations();
 
@@ -61,8 +72,8 @@ public final class XPrisonAPITest extends JavaPlugin {
     private void initEnchants() {
         customEnchants = new ArrayList<>();
 
-        File diamondGiverConfigFile = new File(getDataFolder(), "diamondgiver.json");
-        File invisibilityConfigFile = new File(getDataFolder(), "invisibility.json");
+        File diamondGiverConfigFile = new File(dataFolder, "diamondgiver.json");
+        File invisibilityConfigFile = new File(dataFolder, "invisibility.json");
 
         customEnchants.add(new DiamondGiverEnchant(diamondGiverConfigFile));
         customEnchants.add(new InvisibilityEnchant(invisibilityConfigFile));
@@ -84,29 +95,30 @@ public final class XPrisonAPITest extends JavaPlugin {
 
     /**
      * Copies the default configuration files for the enchantments
-     * from the plugin's resources to the plugin data folder,
+     * from the plugin's resources to the plugins/X-Prison/addon-data/XPrisonAPITestAddon
      * if they do not already exist.
      */
     private void copyDefaultEnchantConfigurations() {
-        File pluginFolder = this.getDataFolder();
-        if (!pluginFolder.exists()) {
-            pluginFolder.mkdirs();
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
         }
 
         String[] enchantFiles = {"diamondgiver.json", "invisibility.json"};
 
         for (String fileName : enchantFiles) {
-            File outFile = new File(pluginFolder, fileName);
+            File outFile = new File(dataFolder, fileName);
+            URL resource = XPrisonAPITestAddon.class.getResource("/" + fileName);
+            System.out.println(resource.toString());
             if (!outFile.exists()) {
-                try (InputStream in = getResource(fileName)) {
+                try (InputStream in = XPrisonAPITestAddon.class.getResourceAsStream("/" + fileName)) {
                     if (in != null) {
                         Files.copy(in, outFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                        getLogger().info("Copied default " + fileName);
+                        Bukkit.getLogger().info("[XPrisonAPITestAddon] Copied " + fileName);
                     } else {
-                        getLogger().warning("Resource not found: " + fileName);
+                        Bukkit.getLogger().warning("[XPrisonAPITestAddon] Could not find " + fileName + " in resources!");
                     }
                 } catch (IOException e) {
-                    getLogger().warning("Failed to copy " + fileName + ": " + e.getMessage());
+                    Bukkit.getLogger().warning("[XPrisonAPITestAddon] Failed to copy " + fileName + ": " + e.getMessage());
                 }
             }
         }
